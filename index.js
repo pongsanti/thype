7a4e11c264369b0deed7c5c988b240a5e80f8c2f
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-11-16 23:45:49 +0700

    render template with pug

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express')
+var app = express()
+
+app.set('views', './views')
+app.set('view engine', 'pug')
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/welcome', function (req, res) {
+  res.render('welcome', { title: 'Hey', message: 'Hello there!' })
+})
+
+app.use(express.static('public'))
+app.use(express.static('bower_components/bulma'))
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
